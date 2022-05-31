9e7f1e35ae6251105e8febddb5e59e2eaf1e48dd
Matt Harvey <matt@teamdawg.org>
2022-05-31 12:27:23 -0700

    tcp_pty checkpoint

--- tcp_pty/src/main.rs
+++ tcp_pty/src/main.rs
@@ -4,11 +4,20 @@ use std::os::unix::prelude::RawFd;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
 
+fn set_nonblocking(fd: RawFd) -> Result<(), nix::Error> {
+    let bits = nix::fcntl::fcntl(fd, nix::fcntl::FcntlArg::F_GETFL)?;
+    let mut flags = nix::fcntl::OFlag::from_bits_truncate(bits);
+    flags.insert(nix::fcntl::OFlag::O_NONBLOCK);
+    nix::fcntl::fcntl(fd, nix::fcntl::FcntlArg::F_SETFL(flags))?;
+    Ok(())
+}
+
 async fn remote_control(
     master: RawFd,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("0.0.0.0:8888").await?;
     let (mut socket, _) = listener.accept().await?;
+    set_nonblocking(master)?;
     let mut ptmx = unsafe { tokio::fs::File::from_raw_fd(master) };
     ptmx.write_all("import os\nos.listdir()\n".as_bytes()).await?;
     tokio::io::copy_bidirectional(&mut socket, &mut ptmx).await?;
@@ -20,7 +29,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     match pty.fork_result {
         ForkResult::Child => {
             return Err(Box::new(
-                exec::Command::new("/usr/bin/python3").exec(),
+                exec::Command::new("python3").exec(),
             ));
         }
         ForkResult::Parent { child: _ } => {
